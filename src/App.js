69a1b01a5e0ec6034419cb63d438ec3a63f02706
Prevergenec <prevergenec@ya.ru>
2023-02-12 20:31:33 +0300

    Second lesson

--- src/App.js
+++ src/App.js
@@ -1,53 +1,43 @@
-import {Component} from 'react';
-import './App.css';
+import { Component } from 'react';
 
+import './App.css'
 
-const Header = () => {
-  return <h2>Hello World!</h2>
-}
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: '+++'
+    }
+  }
 
-// const Field = () => {
-//   const holder = 'Text here';
-//   const styleField = {
-//     width: '300px'
-//   };
+  nextYear = () => {
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
 
-//   return <input 
-//           placeholder={holder} 
-//           type="text"
-//           style={styleField} />
-// }
+  render () {
+    const {name, surname, link} = this.props;
 
-function Btn() {
-  const text = 'Log in';
-  const logged = false;
-
-  return <button>{logged ? 'Enter' : text}</button>
-}
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
+} 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Field />
-      <Btn />
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Shaper" link="vk.com" />
     </div>
   );
 }
 
-class Field extends Component {
-  render() {
-    const holder = 'Text here';
-    const styleField = {
-      width: '300px'
-    };
 
-  return <input 
-          placeholder={holder} 
-          type="text"
-          style={styleField} />
-  }
-}
-
-export {Header};
 export default App;
