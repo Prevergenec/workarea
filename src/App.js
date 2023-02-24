4ec595e11684cbb8703dd773489231193865220e
Prevergenec <prevergenec@ya.ru>
2023-02-24 11:34:04 +0300

    Correct

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+
+import './App.css'
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: '+++',
+      position: ''
+    }
+    // this.nextYear = this.nextYear.bind(this); для обычных функций
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  } 
+
+
+  render () {
+    const {name, surname, link} = this.props;
+    const {position, years} = this.state;
+
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
+        <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInputChanges} />
+        </form>
+      </div>
+    )
+  }
+} 
+
+function App() {
+  return (
+    <div className="App">
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Shaper" link="vk.com" />
+    </div>
+  );
+}
+
+
+export default App;
